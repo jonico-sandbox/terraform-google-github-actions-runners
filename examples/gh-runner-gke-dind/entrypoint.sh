4ec966bfc41b24a6e2cc7895a1f3ee52ee8b2b68
Johannes Nicolai <jonico@github.com>
2021-01-19 01:05:23 +0100

    My modifications to GKE example with custom image

--- examples/gh-runner-gke-dind/entrypoint.sh
+++ examples/gh-runner-gke-dind/entrypoint.sh
@@ -20,7 +20,7 @@ ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 #get regsistration token for this runnner
 ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
 #configure runner
-/runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN"
+/runner/config.sh --labels "k8,gke,kubernetes" --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN"
 #start runner
 #https://github.com/actions/runner/issues/246#issuecomment-615293718
 /runner/bin/runsvc.sh
